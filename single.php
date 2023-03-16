0b745ee30311e67b5ec59f373cfd12870126d530
Cris <cristian.salgado4756@gmail.com>
2023-03-16 11:11:01 -0300

    update

--- single.php
+++ single.php
@@ -1,12 +1,33 @@
 <?php 
+get_header();
+
 while(have_posts()){ 
 the_post();    
 ?>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("./images/ocean.jpg") ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Pagina</p>
+        </div>
+      </div>
 
-<h1><?php the_title(); ?></h1>
+  <div class="metabox metabox--position-up metabox--with-home-link">
+          <p>
+            <a class="metabox__blog-home-link" href="<?php echo site_url("/blog"); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php get_the_title($var); ?></a> 
+            <span class="metabox__main"><?php the_title(); ?></span>
+          </p>
+        </div>
+
+    </div>
+
+    
+
 <?php the_content(); ?>
 
 <?php
 }
 
+get_footer();
 ?>
